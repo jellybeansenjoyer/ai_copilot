20274035af21b5ca05ef92a15c7cae2357634611
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2025-07-06 21:12:16 +0530

    main

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images:{
     domains: ['res.cloudinary.com','lh3.googleusercontent.com','miro.medium.com']
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   
 };
 
